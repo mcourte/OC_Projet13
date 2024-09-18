dcb03d302318dc1b5fa657c08a7ae33d0ad7dff2
Magali Courté <courte.magali@gmail.com>
2024-09-18 14:20:26 +0200

    ajout de sentry dans views e tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from profiles.models import Profile
 from django.urls import reverse
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from unittest.mock import patch, call
+from django.http import Http404
+from .views import index
 
 
 class ProfileModelTests(TestCase):
@@ -35,6 +38,7 @@ class ProfileViewTests(TestCase):
             user=self.user,
             favorite_city='Test City'
         )
+        self.factory = RequestFactory()
 
     def test_profiles_index_view(self):
         """
@@ -52,3 +56,26 @@ class ProfileViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'profiles/profile.html')
         self.assertContains(response, 'Test City')
+
+    @patch('profiles.views.Profile.objects.all')
+    @patch('profiles.views.sentry_log')
+    def test_no_profiles_found(self, mock_sentry_log, mock_all):
+        # Créer un objet de requête HTTP factice
+        request = self.factory.get('/profiles')
+        # Simuler un utilisateur (peut être une instance de User si nécessaire)
+        request.user = None
+
+        # Simuler une liste vide de profils
+        mock_all.return_value = []
+
+        # Exécuter la vue
+        with self.assertRaises(Http404):
+            index(request)
+
+        # Vérifier que la méthode sentry_log a été appelée avec les bons types et messages
+        mock_sentry_log.assert_has_calls([
+            call(error_type='message',
+                 error_message="Requête d'index de profil initiée par l'utilisateur : None."
+                 " URL : /profiles, Méthode : GET, Adresse IP : 127.0.0.1"),
+            call(error_type='message', error_message='Aucun profil trouvé.')
+        ])
--- profiles/views.py
+++ profiles/views.py
@@ -4,10 +4,19 @@ profiles/views.py
 Ce module contient les fonctions de vue Django pour afficher
 les listes de profils utilisateur et les détails des profils individuels.
 """
-
-from django.shortcuts import render, get_object_or_404
+import os
+import sys
+from django.shortcuts import render
 from profiles.models import Profile
 from django.http import Http404
+# Déterminez le chemin absolu du répertoire parent
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(current_dir, '../'))
+
+# Ajoutez le répertoire parent au PYTHONPATH
+sys.path.insert(0, parent_dir)
+
+from sentry_logger import sentry_log
 
 
 def index(request):
@@ -23,11 +32,28 @@ def index(request):
         HttpResponse: La page HTML rendue affichant la liste des profils
         utilisateur.
     """
+    sentry_log(
+        error_type="message",
+        error_message=(
+            f"Requête d'index de profil initiée par l'utilisateur : {request.user}. "
+            f"URL : {request.path}, "
+            f"Méthode : {request.method}, "
+            f"Adresse IP : {request.META.get('REMOTE_ADDR')}"
+        )
+    )
     profiles_list = Profile.objects.all()
     if not profiles_list:
         error = "Aucun profil trouvé."
+        sentry_log(
+            error_type="message",
+            error_message=error
+        )
         raise Http404(error)
     context = {'profiles_list': profiles_list}
+    sentry_log(
+        error_type="message",
+        error_message="Liste des profils récupérée avec succès."
+    )
     return render(request, 'profiles/index.html', context)
 
 
@@ -35,6 +61,15 @@ def profile(request, username):
     """
     Fonction de vue pour afficher les détails du profil d'un utilisateur.
     """
-    profile = get_object_or_404(Profile, user__username=username)
+    try:
+        sentry_log(
+            error_type="message",
+            error_message=f"Requête de profil initiée pour l'utilisateur : {username}")
+        profile = Profile.objects.get(user__username=username)
+    except Profile.DoesNotExist:
+        error = (f"Profil non existant : Le nom d'utilisateur {username} "
+                 "n'existe pas !")
+        raise Http404(error)
+
     context = {'profile': profile}
     return render(request, 'profiles/profile.html', context)
