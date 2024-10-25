07833edb88b8b6eac41533304ebc654fa8334208
Magali Courté <courte.magali@gmail.com>
2024-10-25 15:08:45 +0200

    Mise à jour de Sentry

--- sentry_logger.py
+++ sentry_logger.py
@@ -1,13 +1,22 @@
 import logging
 from sentry_sdk import capture_message
 
+# Configuration du logger
+logger = logging.getLogger(__name__)
+
 
 def sentry_log(error_type: str = "", error_message: str = ""):
-    """log to sentry"""
+    """Log vers Sentry"""
 
     if error_type == "exception":
-        logging.exception(error_message)
+        logger.exception(error_message)  # Capture une exception avec trace complète
     elif error_type == "message":
-        capture_message(error_message)
+        capture_message(error_message, level="info")  # Capture un message d'information
+    elif error_type == "debug":
+        logger.debug(error_message)  # Capture un log de debug
+    elif error_type == "info":
+        logger.info(error_message)  # Capture un log d'information
+    elif error_type == "warning":
+        logger.warning(error_message)  # Capture un log de warning
     else:
-        logging.error(error_message)
+        logger.error(error_message)  # Capture un log d'erreur
